2d4ff1df6a4ddcc3ace02f3eccc8f350887abf5d
jofuentesl <jofuentesle@uoc.edu>
2024-11-20 22:35:36 +0100

    install mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static', 
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
